6e8a44f2ed70b06f77094ce48331457e5096a84a
Leandro <leandrobcrv@gmail.com>
2023-05-10 01:08:51 -0300

    Requisito 06 e 07

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import userController from '../controller/user.controller';
+import userValidate from '../middleware/userValidate';
+
+const userValidators = [
+  userValidate.usernameValidate, 
+  userValidate.vocationValidate,
+  userValidate.levelValidate,
+  userValidate.passwordValidate,
+];
 
 const userRouter = express.Router();
 
-userRouter.post('/', userController.create);
+userRouter.post('/', userValidators, userController.create);
 
 export default userRouter;
